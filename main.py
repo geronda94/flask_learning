10e1d17047ac085d250ffe9e4d711dfb8323a18d
Igor <Iiiliev.igor@gmail.com>
2023-03-01 13:58:29 +0200

    Windows

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from requests_html import HTMLSession
+from flask import Flask, render_template
 
 
-session = HTMLSession()
-res = session.get('https://mail.ru')
-print(res.text)
+
+menu = ['Главная', 'О нас', 'Еще один пункк']
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/index')
+@app.route("/")
+def index():
+    return render_template('index.html', title="Главная страница", menu=menu)
+
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title="О сайте", menu=menu)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
