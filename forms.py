7c87d530f44c987c7dd1c9e4322a97ecdb54f39d
Igor <Iiiliev.igor@gmail.com>
2023-03-10 14:39:14 +0200

    WINDOWS

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[Email("Емэил введен некоректно!")])
@@ -8,7 +9,12 @@ class LoginForm(FlaskForm):
     remember = BooleanField("Не запоминать", default=True)
     submit = SubmitField("Войти")
 
-
+class RegisterForm(FlaskForm):
+    name = StringField("Имя: ", validators=[Length(min=4, max=100, message='Имя должно быть диной от 4 до 100 символов')])
+    email = StringField("Email", validators=[Email("Емэил введен некоректно!")])
+    psw = PasswordField("Пароль", validators=[DataRequired(), Length(min=5, max=100, message="Длинa пароля о 5 до 100 символов")])
+    psw2 = PasswordField("Пароль", validators=[DataRequired(), EqualTo('psw',message='Пароли не совпадают')])
+    submit = SubmitField("Зарегистрироваться")
 
 
 
