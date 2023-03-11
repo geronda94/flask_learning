ea93771ec6a479a5d64c427459ba6452b89b0127
Igor <Iiiliev.igor@gmail.com>
2023-03-11 13:52:56 +0200

    WINDOWS

--- admin/admin2.py
+++ admin/admin2.py
@@ -1,13 +1,12 @@
 import sqlite3
-from flask import Blueprint, request, redirect, url_for, flash, render_template, session, g
+from flask import Blueprint, render_template, url_for, redirect, session, request, flash, g
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
 
 menu = [{'url': '.index', 'title': 'Панель'},
-        {'url': '.listpubs', 'title': 'Список статей'},
         {'url': '.listusers', 'title': 'Список пользователей'},
+        {'url': '.listpubs', 'title': 'Список статей'},
         {'url': '.logout', 'title': 'Выйти'}]
-
 
 def isLogged():
     return True if session.get('admin_logged') else False
@@ -19,10 +18,10 @@ def logout_admin():
     session.pop('admin_logged', None)
 
 
-
 db = None
 @admin.before_request
 def before_request():
+    """Установление соединения с БД перед выполнением запроса"""
     global db
     db = g.get('link_db')
 
@@ -32,17 +31,12 @@ def teardown_request(request):
     db = None
     return request
 
-
 @admin.route('/')
 def index():
     if not isLogged():
         return redirect(url_for('.login'))
 
     return render_template('admin/index.html', menu=menu, title='Админ-панель')
-
-
-
-
 
 @admin.route('/login', methods=["POST", "GET"])
 def login():
@@ -73,47 +67,29 @@ def listpubs():
     if not isLogged():
         return redirect(url_for('.login'))
 
-    lst = []
+    list = []
     if db:
         try:
             cur = db.cursor()
-            cur.execute(f'SELECT title, text, url FROM posts;')
-            lst = cur.fetchall()
+            cur.execute(f"SELECT title, text, url FROM posts")
+            list = cur.fetchall()
         except sqlite3.Error as e:
-            print('Ошибка при извлечении статьи из БД', str(e))
+            print("Ошибка получения статей из БД " + str(e))
 
-    return render_template('admin/listpubs.html', title='Список статей', menu=menu, list=lst)
-
+    return render_template('admin/listpubs.html', title='Список статей', menu=menu, list=list)
 
 @admin.route('/list-users')
 def listusers():
     if not isLogged():
         return redirect(url_for('.login'))
 
-    lst=[]
+    list = []
     if db:
         try:
             cur = db.cursor()
-            cur.execute(f"SELECT name,email FROM users ORDER BY time DESC")
-            lst = cur.fetchall()
+            cur.execute(f"SELECT name, email FROM users ORDER BY time DESC")
+            list = cur.fetchall()
         except sqlite3.Error as e:
-            print('Ошибка при извлечении пользователей из БД', str(e))
+            print("Ошибка получения статей из БД " + str(e))
 
-    return render_template('admin/listusers.html', title='Список пользователей', menu=menu, list=lst)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return render_template('admin/listusers.html', title='Список пользователей', menu=menu, list=list)
