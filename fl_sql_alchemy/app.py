43d5a60681ec6388302055c3f4667d322806b7da
Igor <Iiiliev.igor@gmail.com>
2023-03-11 14:41:47 +0200

    WINDOWS

---
+++ fl_sql_alchemy/app.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///blog.db'
+#app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://user:password@localhost/mydatabase'
+
+db = SQLAlchemy(app)
+
+class Users(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    psw = db.Column(db.String(500), nullable=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<users {self.id}"
+
+
+class Profiles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=True)
+    old = db.Column(db.Integer)
+    city = db.Column(db.String(100))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f"<profiles> {self.id}"
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
